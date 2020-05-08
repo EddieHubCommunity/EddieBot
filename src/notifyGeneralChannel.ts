82cf8d83d193bdd66ec35a6e5275cf88c617110c
David <didicp4@gmail.com>
2020-05-08 14:00:17 +0100

    refactor: check for 'general' channel name if ID isn't provided

--- src/notifyGeneralChannel.ts
+++ src/notifyGeneralChannel.ts
@@ -3,7 +3,8 @@ import { DMChannel, GuildChannel, TextChannel } from "discord.js";
 export const notifyGeneralChannel = (channel: DMChannel | GuildChannel) => {
     const channelName = 'general';
     const generalChannel = channel.client.channels.cache.find(currentChannel =>
-        currentChannel.id === process.env.GENERAL_CHANNEL_ID
+        currentChannel.id === process.env.GENERAL_CHANNEL_ID ||
+        (currentChannel instanceof TextChannel && currentChannel.name === channelName)
     );
     if (!generalChannel)
         return console.error(`${channelName} channel not found. Make sure to setup GENERAL_CHANNEL_ID env variable.`);
