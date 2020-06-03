6bcba427fbfe55caa6783243f96142301ba8db29
Eddie Jaoude <eddie@jaoudestudios.com>
2020-06-03 11:27:40 +0100

    Improvements to when a new member joins

--- src/guildMemberAdd.ts
+++ src/guildMemberAdd.ts
@@ -1,6 +1,15 @@
 import { GuildMember, PartialGuildMember } from "discord.js";
 
-export const guildMemberAdd = (member: GuildMember | PartialGuildMember) => {
+export const guildMemberAdd = async (member: GuildMember | PartialGuildMember) => {
+    if (member.partial) {
+		try {
+			await member.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			return;
+		}
+    }
+
 	const channelName = 'introductions';
 	const channel = member.guild.channels.cache.find((ch) => ch.name === channelName && ch.type === 'text');
 
