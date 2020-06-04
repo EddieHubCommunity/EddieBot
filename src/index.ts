89e2685615b65f900df505df69a6fdb8038adbd1
David <didicp4@gmail.com>
2020-06-04 14:57:12 +0100

    feat(bot): add a presence to the bot

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,23 @@ import { guildMemberAdd } from './guildMemberAdd';
 import { messageReactionAdd } from './messageReactionAdd';
 import { chatty } from './chatty';
 import { notifyGeneralChannel } from './notifyGeneralChannel';
+import config from './config';
 
 // initial client with params
 const client: Client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 // client is ready
-client.once('ready', () => console.info('Ready LOCAL!'));
+client.once('ready', () => {
+    console.info('Ready LOCAL!');
+
+    client.user && client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: `${config.COMMAND_PREFIX}help for help`,
+            type: 'WATCHING',
+        }
+    });
+});
 
 // bot actions
 client.on('channelCreate', notifyGeneralChannel);
