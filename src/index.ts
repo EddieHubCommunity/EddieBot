afdd0686307080dd13fb69d873380cd7ec6547fe
David <didicp4@gmail.com>
2020-06-04 15:39:33 +0100

    fix(lint): fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Client } from 'discord.js';
 import { commands } from './commands';
+import config from './config';
 import { guildMemberAdd } from './guildMemberAdd';
 import { messageReactionAdd } from './messageReactionAdd';
 import { chatty } from './chatty';
 import { notifyGeneralChannel } from './notifyGeneralChannel';
-import config from './config';
 
 // initial client with params
 const client: Client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
@@ -13,13 +13,15 @@ const client: Client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 client.once('ready', () => {
     console.info('Ready LOCAL!');
 
-    client.user && client.user.setPresence({
-        status: 'online',
-        activity: {
-            name: `${config.COMMAND_PREFIX}help for help`,
-            type: 'WATCHING',
-        }
-    });
+    if (client.user) {
+        client.user.setPresence({
+            activity: {
+                name: `${config.COMMAND_PREFIX}help for help`,
+                type: 'WATCHING',
+            },
+            status: 'online'
+        });
+    }
 });
 
 // bot actions
