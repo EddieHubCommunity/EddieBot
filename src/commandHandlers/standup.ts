0fa877684faf81589109878be2ab642dc6ff0efa
Eddie Jaoude <eddie@jaoudestudios.com>
2020-06-04 16:22:37 +0100

    feat(command): standup command #50

---
+++ src/commandHandlers/standup.ts
@@ -0,0 +1,16 @@
+import { Client, MessageEmbed } from 'discord.js';
+
+export const command = async (arg: string, client: Client, embed: MessageEmbed) => {
+    const args = arg.split('||');
+    embed
+        .setTitle('Standup')
+        .setDescription('What I did yesterday and what I will do today')
+        .addField('Yesterday', args[0])
+        .addField('Today', args[1])
+
+    return embed;
+};
+
+export const description = 'Share your standup notes with the community';
+
+export const triggers = ['standup'];
