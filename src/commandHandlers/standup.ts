cc15dcadb72135cc7475b9a457ecdc96f3a176e6
David <didicp4@gmail.com>
2020-06-07 15:31:12 +0100

    fix(standup): delete user's message and add user info in footer
    Issue: #55

--- src/commandHandlers/standup.ts
+++ src/commandHandlers/standup.ts
@@ -1,4 +1,4 @@
-import { MessageEmbed } from 'discord.js';
+import { MessageEmbed, Message } from 'discord.js';
 
 /**
  * This command lets the user create a message for the daily standup in the community. For more information on
@@ -6,13 +6,20 @@ import { MessageEmbed } from 'discord.js';
  *
  * {link https://www.atlassian.com/agile/scrum/standups}
  */
-export const command = async (arg: string, embed: MessageEmbed) => {
+export const command = async (arg: string, embed: MessageEmbed, message: Message) => {
+    // Note: The delete operation requires the permission "MANAGE_MESSAGES".
+    // No need to await for the promise since sending the embed message doesn't depend on this message being deleted.
+    if (message.deletable) {
+        message.delete();
+    }
+
     const args = arg.split('||');
     embed
         .setTitle('Standup')
         .setDescription('What I did yesterday and what I will do today')
         .addField('Yesterday', args[0])
         .addField('Today', args[1])
+        .setFooter(message.author.username, message.author.avatarURL() || undefined)
 
     return embed;
 };
