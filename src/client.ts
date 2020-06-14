8905f2a29c9e349690e40f8b57e9af58af2b4fe3
David <didicp4@gmail.com>
2020-06-14 15:54:58 +0100

    refactor(client): opt-in to Discord Gateway Intents
    Separated the string for non-privileged intents from the privileged ones, to make it clearer the type of intents we are using.
    
    Reason for change: "For now sending intents is optional, but it will become mandatory as of October 7, 2020!"
    
    Source: https://discordjs.guide/popular-topics/intents.html
    
    Issue: #36

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,12 @@
 import { Client } from 'discord.js';
 
-export const client: Client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+// Official docs: https://discord.com/developers/docs/topics/gateway#privileged-intents
+enum PrivilegedIntents {
+    GUILD_PRESENCES = 'GUILD_PRESENCES',
+    GUILD_MEMBERS =  'GUILD_MEMBERS'
+}
+
+export const client = new Client({
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', PrivilegedIntents.GUILD_MEMBERS] }
+});
