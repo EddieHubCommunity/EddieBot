98873ed963a3dc6df6cca09d7503b4f9fb0b854a
David <didicp4@gmail.com>
2020-07-03 19:46:34 +0100

    fix(roles): remove extra space on function definition

--- src/commandHandlers/guild.service.ts
+++ src/commandHandlers/guild.service.ts
@@ -3,7 +3,7 @@ import { Role, Collection } from 'discord.js';
 import { client as discordClient } from '../client';
 import { log } from '../logger';
 
-export async  function getRoles(): Promise<Collection<string, Role>> {
+export async function getRoles(): Promise<Collection<string, Role>> {
     const guildKey = process.env.DISCORD_SERVER_ID;
     if (!guildKey) {
         log.error('ERROR: Couldn\'t get roles list! Missing env. variable DISCORD_SERVER_ID. Please configure that value.');
