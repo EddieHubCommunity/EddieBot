96daa1b48e45106ce49c9008b0b1d671583af188
Eddie Jaoude <eddie@jaoudestudios.com>
2020-07-03 13:28:01 +0100

    fix(timezone): fixed IST timezone #89

--- src/config.ts
+++ src/config.ts
@@ -22,6 +22,6 @@ export default {
     { abbr: 'AEST', zone: 'Australia/Brisbane' },
     { abbr: 'EEST', zone: 'Asia/Beirut' },
     { abbr: 'UTC', zone: 'Europe/London' },
-    { abbr: 'IST', zone: 'Asia/India' },
+    { abbr: 'IST', zone: 'Asia/Kolkata' },
   ]
 };
