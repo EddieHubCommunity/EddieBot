e09d216fc87fa31050dd6781ed1d58fafdbceb5d
David <didicp4@gmail.com>
2020-07-15 22:51:57 +0100

    feat(scheduler): add the reminder command
    Issue: #81

--- src/commandHandlers/index.ts
+++ src/commandHandlers/index.ts
@@ -2,10 +2,11 @@ import * as bio from './bio';
 import * as codeOfConduct from './codeOfConduct';
 import * as help from './help';
 import * as roles from './roles';
+import * as reminder from './reminder';
 import * as standup from './standup';
 import * as stats from './stats';
 import * as tips from './tips/tips';
 
-export default [bio, codeOfConduct, help, roles, standup, stats, tips];
+export default [bio, codeOfConduct, help, reminder, roles, standup, stats, tips];
 
 export { fallback } from './fallback';
