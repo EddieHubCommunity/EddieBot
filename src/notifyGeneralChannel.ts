b191880bab18ec677186dda277c2819b269a3446
David <didicp4@gmail.com>
2020-07-17 13:11:54 +0100

    fix(notifyGeneralChannel): linting style
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/notifyGeneralChannel.ts
+++ src/notifyGeneralChannel.ts
@@ -7,7 +7,9 @@ const { GENERAL_CHANNEL } = config;
 
 export const notifyGeneralChannel = async (channel: DMChannel | GuildChannel) => {
     // Note: When the bot sends DMS to members, this is triggered. So we ignore the notification
-    if (channel instanceof DMChannel) return
+    if (channel instanceof DMChannel) {
+       return;
+    }
 
     const generalChannel = channel.client.channels.cache.find(currentChannel =>
         currentChannel.id === process.env.GENERAL_CHANNEL_ID ||
