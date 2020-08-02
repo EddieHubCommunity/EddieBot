c537228df2dd43a651eccc5b3b19a7f9629eaf0f
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-08-02 11:20:22 -0600

    fix: fixed trailing whitespace

--- src/timezone.ts
+++ src/timezone.ts
@@ -19,7 +19,6 @@ export const timezone = async (message: Message) => {
 
     const match = message.content.match(/([\d]{1,2})([:\d]{3})?[\s]?(pm|am|AM|PM)?\b[\s]([a-zA-Z]{2,3})?\b/);
     
-
     if (match) {
         const [mentioned, hour, minutes, dayNight, zone] = match;
 
