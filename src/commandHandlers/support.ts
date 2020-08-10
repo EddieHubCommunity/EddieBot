8eea557ac44876e41add732260199e601afd6a49
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-08-09 19:23:49 -0600

    fix(support): correct wording for support message

--- src/commandHandlers/support.ts
+++ src/commandHandlers/support.ts
@@ -4,7 +4,7 @@ import { MessageEmbed, Message } from 'discord.js';
 export const command = async(arg: string, embed: MessageEmbed, message: Message) => {
     const mention = message.mentions.users.first();
     embed
-    .addField('Support', `Hey ${mention ? mention!.username : message.author.username}, for help and any suggestions please raise an issue on our GitHub Community`)
+    .addField('Support', `Hey ${mention ? mention!.username : message.author.username}, for help or to submit a suggestion, please raise an issue on our GitHub Community`)
     .addField('Support Issue Repo Link', 'https://github.com/EddieJaoudeCommunity/support/issues');
 
     return embed;
