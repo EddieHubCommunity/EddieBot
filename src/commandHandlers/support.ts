af87fb6777b3da3c7165715c8fa578cdd52f36ed
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-08-22 09:25:29 -0600

    fix(support): adjust style

--- src/commandHandlers/support.ts
+++ src/commandHandlers/support.ts
@@ -3,15 +3,14 @@ import { MessageEmbed, Message } from 'discord.js';
 
 export const command = async(arg: string, embed: MessageEmbed, message: Message) => {
     const mention = message.mentions.users.first();
-    embed
-    .addField('Support', `Hey ${mention ? mention!.username : message.author.username}, for help or to submit a suggestion, please raise an issue on our GitHub Community`)
-    .addField('Support Issue Repo Link', 'https://github.com/EddieJaoudeCommunity/support/issues');
 
-    return embed;
+    return embed
+            .addField('Support', `Hey ${mention ? mention!.username : message.author.username}, for help or to submit a suggestion, please raise an issue on our GitHub Community`)
+            .addField('Support Issue Repo Link', 'https://github.com/EddieJaoudeCommunity/support/issues');
 };
 
 export const description = 'Get Support information';
 
 export const triggers = ['support'];
 
-export const usage = `${triggers[0]} or ${triggers[0]} <@user>`
+export const usage = `${triggers[0]} or ${triggers[0]} <@user>`;
