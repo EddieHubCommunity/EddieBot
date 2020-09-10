fc3d060f1c118db153193a09fa430744803bd4f0
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-09-10 17:05:53 -0600

    fix(opensource.service): fix style

--- src/commandHandlers/tips/opensource.service.ts
+++ src/commandHandlers/tips/opensource.service.ts
@@ -1,20 +1,16 @@
 import { promises as fs } from 'fs';
 import * as path from 'path';
 import config from '../../config';
-// TODO: read the data for tips and resources from the DB. The idea is for this data to not be hardcoded, and can be
-// modified throughout time, without needing to re-deploy the bot!
 
 export async function getOpenSourceTips(): Promise<string> {
-    const result = await readMarkdown('openSourceTips.md');
-    return result;
+    return await readMarkdown('openSourceTips.md');
 }
 
 export async function getOpenSourceResources(): Promise<string> {
-    const result = await readMarkdown('openSourceResources.md');
-    return result;
+    return await readMarkdown('openSourceResources.md');
 }
 
 async function readMarkdown(fileName: string) {
     const filePath = path.resolve(path.join(config.TIPS_DIRECTORY, `/${fileName}`));
-    return await (await fs.readFile(filePath)).toString();
+    return (await fs.readFile(filePath)).toString();
 }
