63353c741424ab4522f7e39893d36335dd49ac6f
Eddie Jaoude <eddie@jaoudestudios.com>
2020-09-22 19:48:16 +0100

    feat(tips): load files from config (#254)

--- src/commandHandlers/tips/opensource.service.ts
+++ src/commandHandlers/tips/opensource.service.ts
@@ -3,17 +3,9 @@ import * as path from 'path';
 import config from '../../config';
 import { log } from '../../logger';
 
-export async function getOpenSourceTips(): Promise<string> {
-    return await readMarkdown('openSourceTips.md');
-}
-
-export async function getOpenSourceResources(): Promise<string> {
-    return await readMarkdown('openSourceResources.md');
-}
-
-async function readMarkdown(fileName: string) {
+export async function readMarkdown(fileName: string) {
     try {
-        const filePath = path.resolve(path.join(config.TIPS_DIRECTORY, `/${fileName}`));
+        const filePath = path.resolve(path.join(config.TIPS.directory, fileName));
         return (await fs.readFile(filePath)).toString();
     } catch(e) {
         log.error(`ERROR: Could not read file: ${fileName} - ${e.message}`);
--- src/commandHandlers/tips/opensource.ts
+++ src/commandHandlers/tips/opensource.ts
@@ -1,18 +1,22 @@
 import { MessageEmbed } from 'discord.js';
 
-import { getOpenSourceResources, getOpenSourceTips } from './opensource.service';
+import config from '../../config';
+import { readMarkdown } from './opensource.service';
 
 /**
  * Set the given embed message with a curated list of tips and resources that help members of the community to
  * contribute to open-source software (OSS).
  */
 export const createTip = async (embed: MessageEmbed) => {
-    const tips = await getOpenSourceTips();
-    const resources = await getOpenSourceResources();
+
+    const tips = await Promise.all(config.TIPS.tips.map((file) => readMarkdown(file)));
+    const resources = await Promise.all(config.TIPS.resources.map((file) => readMarkdown(file)));
+
+    tips.forEach((tip) => embed.addField('Tips :bulb:', tip));
+    resources.forEach((resource) => embed.addField('Links and Resources :link:', resource));
+
     embed
         .setTitle('Open source Tips and Resources')
-        .addField('Tips :bulb:', tips)
-        .addField('Links and Resources :link:', resources)
         .addField('Help us with this list :heart:', 'If you want to contribute to this list, feel free to [open an issue](https://github.com/EddieJaoudeCommunity/EddieBot/issues). Label it with "command: opensource" so we can easily find your tips and links :slight_smile:');
 
     return embed;
