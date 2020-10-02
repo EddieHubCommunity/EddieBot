cffd60fb964c6e3d55f85468e5e92a7d22af2d78
Lucas Pinto <lucks@outlook.pt>
2020-10-02 18:58:08 +0100

    feat(issue): create issue command

--- src/commandHandlers/index.ts
+++ src/commandHandlers/index.ts
@@ -4,11 +4,12 @@ import * as help from './help';
 import * as roles from './roles';
 import * as reminder from './reminder';
 import * as iam from './iam';
+import * as issue from './issue';
 import * as standup from './standup';
 import * as stats from './stats';
 import * as tips from './tips/tips';
 import * as support from './support';
 
-export default [bio, codeOfConduct, help, iam, reminder, roles, standup, stats, tips, support];
+export default [bio, codeOfConduct, help, iam, issue, reminder, roles, standup, stats, tips, support];
 
 export { fallback } from './fallback';
---
+++ src/commandHandlers/issue.ts
@@ -0,0 +1,37 @@
+import { get as axiosGet } from 'axios';
+import { load as cheerioLoad } from 'cheerio';
+import { Message, MessageEmbed } from 'discord.js';
+
+/**
+ * This command lets the user create an issue from discord to a github repository
+ */
+export const command = async (arg: string, embed: MessageEmbed, message: Message) => {
+    message.channel.send('To which repository would you like to create an issue?')
+    try {
+        // await repository name
+        const repository = await message.channel.awaitMessages((m: Message) => m.author.id === message.author.id, { max: 1, time: 10000, errors: ['time'] }).then(collected => {
+            const res = collected.first();
+            if(res) return res.content;
+            else return 'EddieBot';
+        }).catch(() => { throw new Error('You took too long to reply (`10 seconds`). Try again.') });
+
+        let url = 'https://github.com';
+        const { data } = await axiosGet(`https://github.com/search?q=${encodeURI(repository)}`);
+        const $ = cheerioLoad(data);
+        const element = $('a.v-align-middle').first();
+        const attr = element.attr();
+        if(!attr) return `Could not find repository with name \`${repository}\`.`;
+        url += `${attr.href}/issues/new`;
+
+        if(arg) url += `?title=${encodeURI(arg)}`;
+        return `Here you go: <${url}>`;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+export const description = 'Create a link for an issue on a github repository';
+
+export const triggers = ['issue'];
+
+export const usage = `${triggers[0]} <title>`;
