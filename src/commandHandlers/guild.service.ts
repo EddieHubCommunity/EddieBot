3b9d04594d7d106ced5885369eab043974f4c50e
David <didicp4@gmail.com>
2020-10-05 12:33:52 +0100

    style: linting fixes

--- src/commandHandlers/guild.service.ts
+++ src/commandHandlers/guild.service.ts
@@ -7,7 +7,7 @@ export async function getRoles(): Promise<Collection<string, Role>> {
   const guild = getConfiguredGuild();
   if (!guild) {
     log.error(
-      "ERROR: Couldn't get roles list! The guild with the configured DISCORD_SERVER_ID env. variable doesn't exist"
+      'ERROR: Couldn\'t get roles list! The guild with the configured DISCORD_SERVER_ID env. variable doesn\'t exist'
     );
     return new Collection();
   }
