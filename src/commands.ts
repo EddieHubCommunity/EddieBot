8d2617aa250f7d71adb01980c147adf879fdd415
Lucas Pinto <lucks@outlook.pt>
2020-10-08 21:43:03 +0100

    refactor: Abstract cooldowns

--- src/commands.ts
+++ src/commands.ts
@@ -1,21 +1,19 @@
-import { Collection, Message, Snowflake } from 'discord.js';
+import { Message } from 'discord.js';
 
 import commandList, { fallback } from './commandHandlers';
 import config from './config';
+import cooldown from './cooldown';
 
 const { COMMAND_PREFIX, defaultEmbed } = config;
 
-const cooldownCollection = new Collection<Snowflake, number>();
 export const commands = async (message: Message) => {
   if (!message.content.startsWith(COMMAND_PREFIX) || message.author.bot) {
     return;
   }
 
-  // handle cooldown
-  const nextCommand = cooldownCollection.get(message.author.id);
-  if(nextCommand && nextCommand > Date.now()) {
-      message.channel.send(`:clock1: Too fast! Only **1** command each **${config.COOLDOWN_SECONDS}** seconds.`);
-      return;
+  if(cooldown.isCooled(message.author.id)) {
+    message.channel.send(`:clock1: Too fast! Only **1** command each **${config.COOLDOWN_SECONDS}** seconds.`);
+    return;
   }
 
   const args = message.content.slice(COMMAND_PREFIX.length);
@@ -31,6 +29,6 @@ export const commands = async (message: Message) => {
       message.channel.send(await matching.command(command, embed, message));
   } else {
       message.channel.send(await matching.command(args.slice(command.length + 1), embed, message));
-      cooldownCollection.set(message.author.id, Date.now() + config.COOLDOWN_SECONDS * 1000); // add user to cooldown
+      cooldown.setCool(message.author.id);
   }
 };
