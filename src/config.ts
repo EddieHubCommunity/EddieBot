c785da7c6982bd30215623d2a680b5ac46cb2c82
Lucas <30878914+luckstech@users.noreply.github.com>
2020-10-08 22:16:49 +0100

    feat(issue): GitHub issue command (#285)
    * feat(issue): create issue command
    
    * build(github-token): add github token
    
    * Add whitespace and remove comment on .env.example and use embeds
    
    * style: add missing space
    
    PR: #285
    
    * style: add missing space
    
    PR: #285
    
    * style: linting fixes
    
    PR: #285
    
    * docs: add Github token for issue command
    
    * refactor: BOLT04's suggestions
    
    Co-authored-by: David <didicp4@gmail.com>
    Co-authored-by: Lucas <30878914+itsphaze@users.noreply.github.com>
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import { AxiosRequestConfig } from 'axios';
 import { MessageEmbed } from 'discord.js';
 import * as dotenv from 'dotenv';
 
@@ -152,3 +153,11 @@ export const selfAssignableRoles = [
   'typescript',
   'opensource',
 ];
+
+export const issueRequestConfig: AxiosRequestConfig = {
+  url: `https://api.github.com/search/repositories`
+};
+
+if (process.env.GITHUB_TOKEN) {
+  issueRequestConfig.headers = { 'Authorization': `token ${process.env.GITHUB_TOKEN}` };
+}
