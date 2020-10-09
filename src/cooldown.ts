7abefb3c7ac1c1400a49cf545bb7625a11b5e07c
Lucas <30878914+luckstech@users.noreply.github.com>
2020-10-09 20:15:59 +0100

    Update src/cooldown.ts
    Co-authored-by: David <didicp4@gmail.com>

--- src/cooldown.ts
+++ src/cooldown.ts
@@ -6,7 +6,7 @@ const collection = new Collection<Snowflake, number>();
 export default {
     isCooled: (id: Snowflake): boolean => {
         const nextCommand = collection.get(id);
-        return nextCommand ? nextCommand > Date.now() : false;
+        return nextCommand && nextCommand > Date.now();
     },
     setCool: (id: Snowflake): void => {
         collection.set(id, Date.now() + config.COOLDOWN_SECONDS * 1000);
