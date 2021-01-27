b6657c1c17e249a6c86d3938e38ea98dcc98d052
Vyvy-vi <vyom.j@protonmail.com>
2021-01-27 22:47:19 +0000

    feat added (roles -a)
    Introduced a more fragmented structure, where there are multiple ways of accessing the role-list. The default on (roles) is going to be a list of Assignable roles, the (roles -a) or (roles all) would give a list of all roles. (roles <rolename>) would give the description for the role, if it exists.

--- src/commandHandlers/roles.ts
+++ src/commandHandlers/roles.ts
@@ -1,31 +1,44 @@
 import { MessageEmbed } from 'discord.js';
 
 import { getRoles } from './guild.service';
-import config from '../config';
+import config, { selfAssignableRoles } from '../config';
 
 /**
  * This command lists the available roles on the discord server
  */
 export const command = async (arg: [string, string], embed: MessageEmbed) => {
   const roles = await getRoles();
-  const rolesList = roles
-    .filter((r) => !r.name.includes('everyone')) // Filter the default role everyone has access to
-    .map((discordRole) => {
-      const role = Object.values(config.ROLE).find(
-        (r) => r.name === discordRole.name
-      );
-      const roleDescription = role ? `- ${role.description}` : '';
+  const specificRole = arg[1];
+  if (specificRole) {
+    if (arg[1] === '-a' || arg[1] === 'all') {
+      const rolesList = roles
+        .filter((r) => !r.name.includes('everyone')) // Filter the default role everyone has access to
+        .map((discordRole) => {
+          const role = Object.values(config.ROLE).find(
+            (r) => r.name === discordRole.name
+          );
+          const roleDescription = role ? `- ${role.description}` : '';
 
-      return `\n• ${discordRole.toString()} ${roleDescription}`;
-    });
-
-  embed.setTitle('Available Roles')
-    .setDescription(`Here is the list of roles on this server. You can assign almost any role to yourself. Some of the roles are admin only or given to you via a condition!
+          return `${discordRole.toString()} ${roleDescription}`;
+        });
+      embed.setTitle('Available Roles')
+        .setDescription(`Here is the list of all the roles on this server. You can assign almost any role to yourself. Some of the roles are admin only or given to you via a condition!
         ${rolesList}
         Example of usage:
-        ^iam add javascript`);
-
-  return embed;
+        \`^iam add javascript\``);
+      return embed;
+    } else {
+      // Parse the roles list for the argument, and set up a try except,
+      // if anything errors out send an Error Embed
+      console.log('details about the role');
+      return embed;
+    }
+  } else {
+    console.log('weeeee');
+    return embed;
+    // Parse the Assignable-roles list for the argument, and set up a try except,
+    // if anything errors out send an Error Embed
+  }
 };
 
 export const description = 'Server roles';
