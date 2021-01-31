de8fc885867e048acd5cacae1b6c78de717ed3ce
Vyvy-vi <vyom.j@protonmail.com>
2021-01-31 11:16:34 +0000

    refactor(roles.ts): implemented function, cleared mess
    This commit takes into account the suggestions given by @BOLT04(David). This makes the code more
    readable and reduces redundancy in the code.

--- src/commandHandlers/roles.ts
+++ src/commandHandlers/roles.ts
@@ -12,83 +12,72 @@ export const command = async (arg: [string, string], embed: MessageEmbed) => {
 
   if (specificRole) {
     if (arg[1] === '-a' || arg[1] === 'all') {
-      const describedRoles: string[] = [];
-      const rolesList: string[] = [];
-      roles.forEach((role) => {
-        if (role.name.includes('everyone')) {
-          return;
-        }
-        const roleConfig = Object.values(config.ROLE).find(
-          (r) => r.name === role.name
-        );
-        if (roleConfig && roleConfig.description) {
-          describedRoles.push(
-            `\n${role.toString()} - ${roleConfig.description}`
-          );
-        } else {
-          rolesList.push(` ${role.toString()}`);
-        }
-      });
+      const roleInfo = getRoleLists(roles, (r) => r.name.includes('everyone'));
       embed.setTitle('Available Roles')
         .setDescription(`Here is the list of all the roles on this server. You can assign almost any role to yourself. Some of the roles are admin only or given to you via a condition!\n
-        ${rolesList.toString()}
-        ${describedRoles.toString()}\n
+        ${roleInfo[0].toString()}
+        ${roleInfo[1].toString()}\n
         Assigning this role to yourself:
         \`^iam add javascript\``);
-      return embed;
     } else {
       // Search the roles list for the argument, and
       // if the role is not found, send an Error Embed
       const role = arg[1];
-      const roleObject = roles.find((r) => r.name === role);
-      if (roleObject) {
-        const roleData = Object.values(config.ROLE).find(
-          (r) => r.name === role
-        );
-        const roleDescription = roleData ? `- ${roleData.description}` : '';
-        embed.setTitle(`${role} Role`)
-          .setDescription(`\n${roleObject} ${roleDescription}
-          \nAssigning this role to yourself:
-              \`^iam add ${role}\``);
-      } else {
+      const roleInfo = getRoleLists(roles, (r) => !r.name.includes(role));
+      const roleText = [].concat(roleInfo[0], roleInfo[1]).toString();
+      if (roleText.length === 0) {
         embed
           .setColor(config.COLORS.alerts)
           .setTitle('Role Listing (error)')
           .setDescription(`**${role}** \nThat role does not exist`)
           .addField('Usage', usage);
         console.log("ERROR- The queried role can't be found");
+      } else {
+        embed.setTitle(`${role} Role`).setDescription(`${roleText}
+          \nAssigning this role to yourself:
+              \`^iam add ${role}\``);
       }
-      return embed;
     }
   } else {
-    // Parse the Assignable-roles list and display assignable roles
-    const describedRoles: string[] = [];
-    const rolesList: string[] = [];
-    roles.forEach((role) => {
-      if (!selfAssignableRoles.includes(role.name)) {
-        return;
-      }
-      const roleConfig = Object.values(config.ROLE).find(
-        (r) => r.name === role.name
-      );
-      if (roleConfig && roleConfig.description) {
-        describedRoles.push(`${role.toString()} - ${roleConfig.description}\n`);
-      } else {
-        rolesList.push(` ${role.toString()}`);
-      }
-    });
+    const roleInfo = getRoleLists(
+      roles,
+      (r) => !selfAssignableRoles.includes(r.name)
+    );
     embed.setTitle('Assignable Roles')
       .setDescription(`Here is the list of all self-assignable roles on this server. You can assign the following roles to yourself:\n
-      ${rolesList}\n
-      ${describedRoles.toString()}
+      ${roleInfo[0].toString()}
+      ${roleInfo[1].toString()}\n
       Assigning roles to yourself:
       \`^iam add javascript\``);
-    return embed;
   }
+  return embed;
 };
+
+// Returns two lists, one with all the role names and the other with role names followed by their description.
+function getRoleLists(
+  roles: Collection<string, Role>,
+  isValidRole: (role: Role) => boolean
+): [string[], string[]] {
+  const describedRoles: string[] = [];
+  const rolesList: string[] = [];
+  roles.forEach((role) => {
+    if (isValidRole(role)) {
+      return;
+    }
+    const roleConfig = Object.values(config.ROLE).find(
+      (r) => r.name === role.name
+    );
+    if (roleConfig && roleConfig.description) {
+      describedRoles.push(`\n${role.toString()} - ${roleConfig.description}`);
+    } else {
+      rolesList.push(` ${role.toString()}`);
+    }
+  });
+  return [rolesList, describedRoles];
+}
 
 export const description = 'Server roles';
 
 export const triggers = ['roles'];
 
-export const usage = triggers[0] + ' <argument>';
+export const usage = triggers[0] + ' <>';
