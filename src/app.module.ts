eb8c0467f9c1beb3613c1d76218672775f88419a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-05-08 08:35:37 +0200

    style: consistent naming

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DiscordModule } from 'discord-nestjs';
 import { AppService } from './app.service';
 import { DiscordConfigService } from './environment/discord-config.service';
-import { AlexModule } from './alexjs/alexjs.module';
+import { AlexModule } from './alexjs/alex.module';
 
 @Module({
   imports: [
