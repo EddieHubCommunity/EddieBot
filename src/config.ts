7a51d59308ff57b54b3524412af86f15f3e4f13b
Anirudh  Anup <anirudh012009@gmail.com>
2021-08-17 13:17:55 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { MessageEmbed } from 'discord.js';
 
 export default {
-  preventWords: ['guyz', 'guyzz', 'bruh', 'duude', 'women', 'sir', 'man','`guyz`', '`guyzz`', '`bruh`', '`duude`', '`women`', '`sir`', '`man`'],
+  preventWords: ['guyz', 'guyzz', 'bruh', 'duude', 'women', 'sir', 'man',],
   alexWhitelist: {
     profanitySureness: 1,
     noBinary: true,
