721dba81dd5bb7071da8490a5ef3996d054db4b5
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-09-04 12:09:15 +0530

    Added bitch into prevented words

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,7 @@ export default {
     'yessir',
     'chad',
     'simp',
+    'bitch'
   ],
   // words we allow even if AlexJS blocks (words are sometimes grouped by we want to be more granular)
   allowedWords: ['fellow'],
