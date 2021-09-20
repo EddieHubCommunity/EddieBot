2fb4c018825a95de86c40e05095f70fcd319b7b5
Hitesh Kowdiki <61322830+kkhitesh@users.noreply.github.com>
2021-09-20 10:13:05 +0530

    feat: :rocket: Added dork to block list(#659)

--- src/config.ts
+++ src/config.ts
@@ -22,6 +22,7 @@ export default {
     'chad',
     'simp',
     'mate',
+    'dork',
   ],
   // words we allow even if AlexJS blocks (words are sometimes grouped by we want to be more granular)
   allowedWords: ['fellow'],
