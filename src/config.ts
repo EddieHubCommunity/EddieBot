1ff0e96246da99096c04d40065a7ab85979084d9
Krish Gupta <krishguptadev@outlook.com>
2021-10-09 20:30:18 +0530

    style: lint

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,6 @@ export default {
     'mate',
     'sis',
     'dork',
-
   ],
   // words we allow even if AlexJS blocks (words are sometimes grouped by we want to be more granular)
   allowedWords: ['fellow'],
