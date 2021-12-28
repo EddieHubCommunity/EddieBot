14cdbd90724cb755e2e1e7f2395d1c9ef6786354
JPSO <jpso.guerreiro@gmail.com>
2021-12-28 17:37:59 +0000

    linter fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,18 +3,14 @@ import { On } from 'discord-nestjs';
 import chalk from 'chalk';
 import fs from 'fs';
 
-process.on("error", (e) => {
-    console.log(
-      chalk.red("Error: ")+
-      chalk.bold(e.code)+ 
-      ": " +
-      e.message
-      );
-      let etime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')  
-      let fname = ".httpserver-"+etime.split(" ")[0]+"-"+etime.split(" ")[1]
-    fs.writeFileSync(fname,JSON.stringify(e));
-    process.exit(1)
-})
+process.on('error', (e) => {
+  console.log(chalk.red('Error: ') + chalk.bold(e.code) + ': ' + e.message);
+  const etime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  const fname =
+    '.httpserver-' + etime.split(' ')[0] + '-' + etime.split(' ')[1];
+  fs.writeFileSync(fname, JSON.stringify(e));
+  process.exit(1);
+});
 
 @Injectable()
 export class AppService {
