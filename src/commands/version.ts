d30e5c94c4eebc7f6aab59700f56cdcb703843f1
Eddie Jaoude <eddie@jaoudestudios.com>
2022-07-17 14:21:35 +0100

    feat: version command

---
+++ src/commands/version.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbed } from 'discord.js';
+import { Command } from '../interfaces/Command';
+import { errorHandler } from '../utils/errorHandler';
+
+export const version: Command = {
+  data: new SlashCommandBuilder()
+    .setName('version')
+    .setDescription('Display version info about our bot.'),
+  run: async (bot, interaction) => {
+    try {
+      await interaction.deferReply();
+
+      const embed = new MessageEmbed();
+      embed.setTitle('Introducing EddieBot!!!');
+      embed.setDescription(`Currently running version ${process.env.ENV}`);
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (err) {
+      await errorHandler(bot, err, 'version command');
+    }
+  },
+};
