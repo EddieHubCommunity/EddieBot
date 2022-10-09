413a13c4c3da5a339db8b1fea83ab52bcd43089f
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-09 11:12:06 +0100

    fix: debug mongo ssl cert

--- src/database/connectDb.ts
+++ src/database/connectDb.ts
@@ -6,9 +6,8 @@ import { errorHandler } from '../utils/errorHandler';
 export const connectDb = async (bot: ExtendedClient) => {
   // DigitalOcean Apps has cert as environment variable but Mongo needs a file path
   // Write Mongo cert file to disk
-  console.log('DB string ...', process.env.API_MONGO_CONNECTION_STRING);
-  console.log('DB CERT ...', process.env.CA_CERT);
-  fs.writeFileSync('cert.pem', process.env.CA_CERT!);
+  console.log('DB CERT ...', process.cwd());
+  fs.writeFileSync('/cert.pem', process.env.CA_CERT!);
 
   try {
     await connect(bot.config.dbUri);
