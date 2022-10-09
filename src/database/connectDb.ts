f7868249e7af04f3858b1d1d58b6ce05547934ea
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-09 09:56:05 +0100

    fix: mongo cert file

--- src/database/connectDb.ts
+++ src/database/connectDb.ts
@@ -1,10 +1,15 @@
 import { connect } from 'mongoose';
 import { ExtendedClient } from '../interfaces/ExtendedClient';
 import { errorHandler } from '../utils/errorHandler';
+import fs from 'fs';
 
 export const connectDb = async (bot: ExtendedClient) => {
+  // DigitalOcean Apps has cert as environment variable but Mongo needs a file path
+  // Write Mongo cert file to disk
+  fs.writeFileSync('cert.pem', process.env.CA_CERT!);
+
   try {
-    await connect(bot.config.dbUri);
+    await connect(`${bot.config.dbUri}&tlsCAFile=cert.pem`);
   } catch (err) {
     await errorHandler(bot, err, 'database connection');
   }
