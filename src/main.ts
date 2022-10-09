64d460429abeedb90b509200233623417839ef4c
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-09 12:36:37 +0100

    fix: health check endpoint

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { loadCommands } from './utils/loadCommands';
 import { registerCommands } from './utils/registerCommands';
 import { validateEnv } from './utils/validateEnv';
 
+import express from 'express';
+const app = express();
+const port = 8080;
+
 (async () => {
   const bot = new Client({
     intents: IntentOptions,
@@ -25,4 +29,13 @@ import { validateEnv } from './utils/validateEnv';
     .login(process.env.DISCORD_TOKEN)
     .catch((err) => errorHandler(bot, err, 'login'));
   await registerCommands(bot);
+
+  // used for DigitalOcean's app health check otherwise it kills the app
+  app.get('/', (req, res: any) => {
+    res.send('Discord bot is running');
+  });
+
+  app.listen(port, () => {
+    console.log('API is running');
+  });
 })();
