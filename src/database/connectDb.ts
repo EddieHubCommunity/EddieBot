ea8a7a496aed6bb33ff5f7718e8ee06e5142cd28
Eddie Jaoude <eddie@jaoudestudios.com>
2024-05-07 10:21:00 +0100

    fix: mongo does not need cert file

--- src/database/connectDb.ts
+++ src/database/connectDb.ts
@@ -6,7 +6,7 @@ import { errorHandler } from '../utils/errorHandler';
 export const connectDb = async (bot: ExtendedClient) => {
   // DigitalOcean Apps has cert as environment variable but Mongo needs a file path
   // Write Mongo cert file to disk
-  fs.writeFileSync('cert.pem', process.env.CA_CERT!);
+  // fs.writeFileSync('cert.pem', process.env.CA_CERT!); no longer needed should remove
 
   try {
     await connect(bot.config.dbUri);
