68f72108580a382dceb8912449a28682efb474db
Dan <35927536+dan-mba@users.noreply.github.com>
2024-06-27 14:50:43 -0400

    fix: fix jest esm setup

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  verbose: true,
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+export default config;
